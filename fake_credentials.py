1933e20e7d2560aef4ff585a19bac4bbeb63ba93
rusti <mrgregory1@gmail.com>
2020-03-24 15:56:00 -0400

    added more emails

---
+++ fake_credentials.py
@@ -0,0 +1,6 @@
+# remove the 'fake_' charactes from the name of this file and replace fake info with yours
+
+gmail_user = 'where_to_check@gmail.com'
+gmail_password = 'wacky_password' 
+
+me = 'where_to_send@gmail.com'
--- fetch_gmail.py
+++ fetch_gmail.py
@@ -8,13 +8,16 @@ import imaplib
 import os
 import sys
 
-from credentials import gmail_password, gmail_user
+from credentials import gmail_password, gmail_user, me
+
+import yagmail
+
+
+
+
 
 def main():
-    """
-    You will need to store your email password in your environment, e.g.
-    export mailpwd=password
-    """
+    
     imap = ImapClient(recipient=gmail_user)
     imap.login()
     # retrieve messages from a given sender
@@ -28,7 +31,46 @@ def main():
         # you could delete them after viewing
         # imap.delete_message(msg['num'])
     # when done, you should log out
+
+    messages_2 = imap.get_messages(sender='no_reply@pcgus.com')
+
+
     imap.logout()
+
+    # process vcat raw message in to three seperate lines
+    for dic in messages:
+        dic['body']=dic['body'].replace('\r','')
+        dic['body']=dic['body'].replace('\n\n','\n')
+        dic['body']=dic['body'].split('\n')
+
+    # find line with success in it and bold it
+    for dic in messages:
+        # if 'VCAT' in dic['body'][0]:
+        for count,string in enumerate(dic['body'],0):
+            if 'successful' in string:
+                dic['body'][count] = '<b>' + string + '</b>'
+
+
+
+
+    # prepare variable for sending
+    contents = []
+
+    # add VCAT messages to contents
+    for msg in messages:
+        for line in msg['body']:
+            contents.append(line)
+        contents.append('\n<hr>\n')
+    for msg in messages_2:
+        contents.append(msg['body'])
+
+    yag = yagmail.SMTP(gmail_user, gmail_password)
+
+    # contents = [
+    #     "This is the body, and here is just text http://somedomain/image.png",
+    #     "You can find an audio file attached.", '/local/path/to/song.mp3'
+    # ]
+    yag.send(me, 'PS Report Summary', contents)
 
 
 if __name__ == "__main__":
